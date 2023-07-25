044c2b9638c7809d8227533de5a50327b0d75405
Elijah Cannon <elijah.cannon@octanner.com>
2023-07-24 18:34:50 -0600

    Initial Commit

---
+++ CS3260FinalProject/CS3260FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CS3260FinalProjectApp.swift
+//  CS3260FinalProject
+//
+//  Created by Elijah C. Cannon on 7/24/23.
+//
+
+import SwiftUI
+
+@main
+struct CS3260FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
